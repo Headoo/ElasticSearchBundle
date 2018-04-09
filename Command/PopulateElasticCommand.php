33c7acce27760025492ca55b76c3d86cd9b935fb
Nicolas Bonnici <nicolasbonnici@gmail.com>
2018-04-09 15:13:25 +0200

    1 line reverted

--- Command/PopulateElasticCommand.php
+++ Command/PopulateElasticCommand.php
@@ -37,7 +37,7 @@ class PopulateElasticCommand extends AbstractCommand
     {
         $this->init($input, $output);
 
-        if ($input->getOption('where') && ! $input->getOption('id')) {
+        if ($input->getOption('where') && $input->getOption('id')) {
             $output->writeln("<error>The option 'where' cannot be used with option 'id'</error>");
             return self::EXIT_FAILED;
         }
